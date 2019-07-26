68cabc4dda197e0487a3095c8ad080e1b7d950b9
lrpatil <patillaxmikant72@gmail.com>
2019-07-26 11:59:35 +0530

    adding files

--- scripts/InstallPython.sh
+++ scripts/InstallPython.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Python3 Installation in Amazon Linux 1
+#Python3 Installation in Amazon Linux 2
 sudo yum install python37 -y
 sudo echo 'alias python="/usr/bin/python37"' >> ~/.bashrc
 sudo echo 'export PATH=/usr/bin:$PATH' >> ~/.bashrc
